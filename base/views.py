27c5f75ba9a0870791ad3011a43d0269554064d4
unknown <danica.juric@fsre.sum.ba>
2021-06-23 20:45:21 +0200

    added login and logout functionality

--- base/views.py
+++ base/views.py
@@ -1,31 +1,43 @@
 from typing import List
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
-class TaskList(ListView):
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+        
+#mixin se mora dodati prije listview zbog restrikcija - nakon ovoga se ide u settings.py u todo_list kako bi se promijenile django unaprijed zadane vrijednosti na one koje nama odgovaraju
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name='tasks'
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name='task'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
     model= Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
