699308fe810f6c4bd070ccd91a37e512bff29f0c
unknown <danica.juric@fsre.sum.ba>
2021-06-24 16:38:55 +0200

    added user registration form

--- base/views.py
+++ base/views.py
@@ -1,13 +1,15 @@
 from typing import List
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from .models import Task
 
 class CustomLoginView(LoginView):
@@ -18,6 +20,23 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
         
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user=form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
 #mixin se mora dodati prije listview zbog restrikcija - nakon ovoga se ide u settings.py u todo_list kako bi se promijenile django unaprijed zadane vrijednosti na one koje nama odgovaraju
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
